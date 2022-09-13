0b65e61c598adb12b9497927f294a840ab2a679d
Mamurooyiboluawhore <missmarvelous2016@gmail.com>
2022-09-12 19:38:23 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,17 +1,5 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
-int main(void)
-{
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	return (0);
-}
 #include <stdio.h>
 /**
  * main - peint if the number is positive, zero, or negative
@@ -27,11 +15,11 @@ int n;
 strand (time(0));
 n= rand() -RAND_MAX / 2;
  /* your code goes there */
-if (n >0)
+if (n > 0)
 {
 	printf("% is positive\n",n);
 }
-else if (n == 0)
+else if (n = = 0)
 {
        	printf("% is zero\n",n);
 }
@@ -41,6 +29,3 @@ else if (n < 0)
 }
 return(0)
 }
-
-
-
