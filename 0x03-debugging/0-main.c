8e74e245839a3654c1f2af247898f70ef82642ca
Mamurooyiboluawhore <missmarvelous2016@gmail.com>
2022-09-14 19:40:44 -0700

    0x03-debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,24 @@
-#include "main_h"
+#include <stdio.h>
+
 /**
- * main - prints positive or negative integer
+ * main - causes an infinite loop
  * Return: 0
- */
+*/
+
 int main(void)
 {
-		 int i;
-		 i = 0;
+	int i;
 
-		 positive_or_negative(i);
-		 return (0);
+	printf("Infinite loop incoming :(\n");
+
+	 i = 0;
+
+	 /*while (i < 10)*/
+	 /*{*/
+	 /*	putchar(i);*/
+	 /*}*/
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
 }
-
