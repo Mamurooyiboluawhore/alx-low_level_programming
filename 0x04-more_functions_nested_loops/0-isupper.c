418226a40ca6bff50e83960701998de758bd8f2e
Mamurooyiboluawhore <missmarvelous2016@gmail.com>
2022-09-15 19:43:06 -0700

    functions_nested_loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,15 @@
-#include "main.h"
 /**
- *_isupper - Checks for uppercase characters
- *@c: Takes a character as input
- * Return: 1 for uppercase, 0 for otherwise
- */
+ * _isupper - check if c is upper
+ *
+ * @c: input for alphabet
+ *
+ * Return: 1 if its uppercase, 0 if not
+*/
+
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if (c >= 65 && c <= 90)
 		return (1);
 	else
 		return (0);
 }
-
--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,13 @@
-#include "main.h"
 /**
- * mul - Checks to multiply two numbers
- * @c: Takes character as input
- * Return: multiplies two numbers
- */
+ * mul - multiplies two integers
+ *
+ * @a: first input
+ * @b: second input
+ *
+ * Return: return results
+*/
 
-int mul(int c)
-
+int mul(int a, int b)
 {
 	return (a * b);
 }
