2e4fb946a2ee3aa77767bd2ba689f2a3299a3eba
Mamurooyiboluawhore <missmarvelous2016@gmail.com>
2022-09-16 18:47:15 -0700

    square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -15,7 +15,7 @@ void print_square(int size)
 	if (size <= 0)
 
 	{
-		_putchar(\n);
+		_putchar('\n');
 	}
 
 	else
@@ -26,7 +26,7 @@ void print_square(int size)
 		{
 			_putchar(35);
 		}
-		_putchar(\n);
+		_putchar('\n');
 	}
 	}
 }
