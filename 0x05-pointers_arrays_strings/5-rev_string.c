ba9a7f0875760573d6e55d70af98414f331bd130
Mamurooyiboluawhore <missmarvelous2016@gmail.com>
2022-09-20 16:36:55 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * rev_string - Reverses a string
+ * @s: Stores input string
+ * Retun: Reversed string
+ */
+
+void rev_string(char *s)
+{
+char rev = s[0];
+int count;
+int j;
+
+count = 0;
+while (s[count] != '\0')
+{
+count++;
+}
+for (j = 0; j < count ; j++)
+{
+count--;
+rev = s[j];
+s[j] = s[count];
+s[count] = s[rev];
+}
+}
