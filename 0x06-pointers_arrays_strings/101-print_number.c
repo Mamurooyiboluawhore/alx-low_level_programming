6b3bebabed0ca4e57524325bb0933c85def57400
Mamurooyiboluawhore <missmarvelous2016@gmail.com>
2022-09-22 11:59:50 -0700

    infinite-add

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,12 +1,24 @@
 #include "main.h"
-/**
- * print_number - Prints something
- * @n: imteger
- * Return: nothing
- *
- */
 
+/**
+ * print_number - print an integer
+ * @n: the integer to print
+ *
+ * Return: void
+ */
 void print_number(int n)
 {
-return (0);
+if (n > -1)
+{
+if (n > 9)
+print_number(n / 10);
+_putchar('0' + n % 10);
+}
+else
+{
+_putchar('-');
+if (n < -9)
+print_number(n / -10);
+_putchar('0' - n % 10);
+}
 }
